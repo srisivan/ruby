3dddaa0060a2690176f90b4adf2dfadf108e0733
Srisivan <srisivan.k@gmail.com>
2017-06-09 19:30:54 +0530

    a simple programming language

--- hello_to_you.rb
+++ hello_to_you.rb
@@ -1,6 +1,10 @@
 
+# Asking an input from the user.
 print "Hello! What's your name?"
+
+# Setting the input to the "name" variable.
 name = gets.chomp
 
+# Displaying the message with the name. 
 puts "Hello, #{name}! My name is Ruby! I'm a general purpose programming language!"
 
