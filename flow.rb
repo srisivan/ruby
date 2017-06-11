06553803fb123b2ea11bb85ca7e0ee5ee2cb4e13
Srisivan <srisivan.k@gmail.com>
2017-06-11 08:19:50 +0530

    added comments

--- flow.rb
+++ flow.rb
@@ -1,8 +1,10 @@
-
+# First we initialize the rate of the water.
 flow_rate = 100
 
+# With the "#{}" synatax, we add the variable to the answer.
 puts "OMG! The flowmatic's rate is #{flow_rate}!"
 
+# We then add the new rate to the variable and add it to the answer. 
 flow_rate = 100 / 2
 
 puts "NOW The rate is #{flow_rate}. (thank goodness)."
