897835470e9456bb81f31b7d0550a7857ea89a65
Srisivan <srisivan.k@gmail.com>
2017-08-23 16:46:05 +0530

    Trials

--- ruby_trials.rb
+++ ruby_trials.rb
@@ -1,25 +1,7 @@
 
-puts "Enter your name:"
-user_name = gets.chomp
-prompt = '>'
+filename = ARGV.first
 
-puts "Hi #{user_name}."
-puts "I'd like to ask you a few questions."
-puts "Do you like me, #{user_name}?"
-puts prompt
+txt = open(filename)
 
-likes = $stdin.gets.chomp
-
-puts "Where do you live, #{user_name}?"
-puts prompt
-
-place = $stdin.gets.chomp
-
-puts "which computer do you have at home?", prompt
-computer = $stdin.gets.chomp
-
-
-puts """
-Alright, you said #{likes} about liking me. You live in #{place}, not sure where that is. You also said that you have a #{computer} at home.
-Nice.
-"""
+puts "Here's your file: #{filename}"
+puts txt.read
