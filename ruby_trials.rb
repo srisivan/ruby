95d77812861eca0589fac123ddfae92006df53ff
Srisivan <srisivan.k@gmail.com>
2017-08-23 16:41:58 +0530

    A trial

--- ruby_trials.rb
+++ ruby_trials.rb
@@ -1,6 +1,25 @@
 
-breakfast = ['Jam', 'Bread', 'Coffee', 'Sausage', 'Fried Rice', 'Noodles']
+puts "Enter your name:"
+user_name = gets.chomp
+prompt = '>'
 
-breakfast.insert(2, 'Sandwich')
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me, #{user_name}?"
+puts prompt
 
-puts( breakfast)
+likes = $stdin.gets.chomp
+
+puts "Where do you live, #{user_name}?"
+puts prompt
+
+place = $stdin.gets.chomp
+
+puts "which computer do you have at home?", prompt
+computer = $stdin.gets.chomp
+
+
+puts """
+Alright, you said #{likes} about liking me. You live in #{place}, not sure where that is. You also said that you have a #{computer} at home.
+Nice.
+"""
